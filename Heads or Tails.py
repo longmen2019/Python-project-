5564d7e9a3bf03096bb7e6622d12e61b2c95efd5
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-12-09 12:02:39 -0500

    Add files via upload

---
+++ Heads or Tails.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 12:01:32 2021
+
+@author: men_l
+"""
+
+
+import random 
+random = random.randint(0,1)
+# print(random)
+
+if random == 1:
+  print("Head")
+else:
+  print("Tail")
