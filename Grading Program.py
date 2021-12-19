473d98bdc9c5a6c4ae8327272e5dd76572c46b49
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-12-19 12:46:52 -0500

    Add files via upload

---
+++ Grading Program.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 19 09:44:05 2021
+
+@author: men_l
+"""
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+
+student_grades = {}
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+
+for student in student_scores:
+    # print(student)
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades [student] = "Exceed Expectations"
+    elif score > 70:
+        student_grades [student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+        
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
