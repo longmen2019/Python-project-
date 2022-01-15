b3acbefd5ea004baf6e7678049f67843f0bd7ccd
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2022-01-14 21:16:15 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
-#Add 
-def add(n1, n2):
-  return n1 + n2
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scorecard import ScoreCard
+import math
+import time
 
+turtle = Turtle()
+snake = Snake()
+food = Food()
+scorecard = ScoreCard()
 
-#Subtract 
-def subtract  (n1,n2):
-  return n1 -n2 
+screen = Screen()
+# Screen setup
+screen.title("Snake Game")
+screen.screensize(500, 500)
+screen.bgcolor("black")
+screen.tracer(0)
 
-#Multiply 
-def multiply (n1,n2):
-  return n1 * n2 
+snake.create_snake()
+game_is_on = True
 
-#Divide 
-def divide (n1,n2):
-  return n1 / n2 
+screen.listen()
+screen.onkey(snake.move_up, "w")
+screen.onkey(snake.move_down, "s")
+screen.onkey(snake.turn_left, "a")
+screen.onkey(snake.turn_right, "d")
 
-operations = {
- "+": add,
-  "-": subtract,
- "*": multiply,
-" /": divide
-}
+food.random_position()
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()  # This refresh has to be last otherwise the score will be added twice
+        snake.adding_segment()  # Adding a new segment to the snake
+        scorecard.score_increase()
 
-for symbol in operations:
-  print (symbol)
+    # Detect collision with the walls
+    elif snake.head.xcor() > 300 or snake.head.ycor() > 300 or \
+            snake.head.xcor() < -300 or snake.head.ycor() < -300:
+        game_is_on = False
+        scorecard.game_over()
+    else:
+        for segment in snake.segments[1:]:
+            #     if segment == snake.head:
+            #         pass
+            #     elif snake.head.distance(segment) < 10:
+            #         game_is_on = False
+            #         scorecard.game_over()
+            if snake.head.distance(segment) < 10:  # Use slicing method
+                game_is_on = False
+                scorecard.game_over()
 
-operation_symbol = input ("Pick an operation from the line above: ")
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1,num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+screen.exitonclick()
